7c29ef3ad770ab510af004f8dedd731da5318e46
charlietran <clvtran@gmail.com>
2020-12-02 14:00:34 -0500

    Incorporating routes.

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const routes = require('./routes/routes.js')(app, fs);
+
 const server = app.listen(8080, () => {
     console.log('listening on port %s...', server.address().port);
   });
