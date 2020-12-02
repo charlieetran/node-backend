23b13aec751a804576217aca0f8d96bc115722ba
charlietran <clvtran@gmail.com>
2020-12-02 13:57:59 -0500

    Adding file system helper.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
+
+// file system helper
+const fs = require('fs');
 
 const server = app.listen(8080, () => {
     console.log('listening on port %s...', server.address().port);
