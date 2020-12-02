5e2182156f0039160201fb3339c6cb5eb448e069
charlietran <clvtran@gmail.com>
2020-12-02 13:59:20 -0500

    Adding body-parser settings to handle JSON data.

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var app = express();
 // file system helper
 const fs = require('fs');
 
+// configure our express instance with some body-parser settings
+// including handling JSON data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const server = app.listen(8080, () => {
     console.log('listening on port %s...', server.address().port);
   });
