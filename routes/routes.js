90a522bba2e46fd1f6499451d7f91c3c5ff76163
charlietran <clvtran@gmail.com>
2020-12-02 14:07:32 -0500

    Connecting users route to route handler.

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,16 @@
 const appRouter = (app, fs) => {};
 
+const userRoutes = require('./users');
+
+const appRouter = (app, fs) => {
+
+  app.get('/', (req, res) => {
+    res.send('Backend Testing');
+  });
+
+  userRoutes(app, fs);
+};
+
 module.exports = appRouter;
+
+
