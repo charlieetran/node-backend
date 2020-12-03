07fb5d82ad41d133f3dd817e2628dc23a88beaf6
charlietran <clvtran@gmail.com>
2020-12-02 21:56:19 -0500

    Implementing mongoose.

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const mongo = require('mongodb').MongoClient
+const mongoose = require('mongoose')
 
 var app = express();
 app.use(cors())
 
 // file system helper
 const fs = require('fs');
-const { MongoClient } = require('mongodb');
 
 // configure our express instance with some body-parser settings
 // including handling JSON data
@@ -18,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const routes = require('./routes/routes.js')(app, fs);
 
 const url = 'mongodb://localhost:27017'
-MongoClient.connect(url, function(err, db) {
+mongoose.connect(url, function(err, db) {
     console.log("Databse connection established");
     db.close();
 })
