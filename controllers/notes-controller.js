d396750b0f14a33177264e56fdce6db121def777
charlietran <clvtran@gmail.com>
2020-12-03 12:04:49 -0500

    Adding notes controller framework.

---
+++ controllers/notes-controller.js
@@ -0,0 +1,18 @@
+const { update } = require('../models/notes-model.js');
+const Note = require('../models/notes-model.js');
+
+exports.create = (req, res) => {
+
+};
+
+exports.read = (req, res) => {
+ 
+};
+
+exports.update = (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+ 
+};
--- routes/notes-route.js
+++ routes/notes-route.js
@@ -1,14 +1,11 @@
 module.exports = (app) => {
-    const notes = require('../controllers/note.controller.js');
+    const notes = require('../controllers/notes-controller.js');
 
     // Create a new Note
     app.post('/notes', notes.create);
 
-    // Retrieve all Notes
-    app.get('/notes', notes.findAll);
-
-    // Retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
+    // Retrieve a Note with noteId
+    app.get('/notes/noteId', notes.read);
 
     // Update a Note with noteId
     app.put('/notes/:noteId', notes.update);
