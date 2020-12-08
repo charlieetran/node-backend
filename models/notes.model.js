1847ffb106fdab405c5fd433fd17c2ee87ec3a6c
charlietran <clvtran@gmail.com>
2020-12-07 23:27:35 -0500

    Created model functions with custom errors.

--- models/notes.model.js
+++ models/notes.model.js
@@ -4,4 +4,126 @@ const NoteSchema = mongoose.Schema({
     content: { type: String, required: true}
 });
 
-module.exports = mongoose.model('Note', NoteSchema);
+const Note = mongoose.model('Note', NoteSchema)
+
+// create functions to move out mongoose functions from controller to here
+// create and handle custom errors
+
+class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ValidationError";
+    }
+}
+
+class DatabaseError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "DatabaseError";
+    }
+}
+
+createNote = async (reqContent) => {
+    if(!reqContent) {
+        throw new ValidationError("No content given");
+    }
+
+    const note = new Note({
+        content: reqContent
+    });
+
+    note.save()
+    .catch(err => {
+        throw err.name || new DatabaseError("Problem saving note");
+    })
+    
+    return note;
+};
+
+
+ findNotes = async () => {
+    return Note.find()
+    .catch(err => {
+        throw err.name || new DatabaseError("Problem finding all notes");
+    })
+    
+};
+
+
+findNote = async (id) => {
+    return Note.findById(id)
+    .then(note => {
+        // how to throw this error?
+        if(note === null) {
+            throw new ValidationError("No note with id: " + id);
+        } 
+        return note;
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            throw new ValidationError("invalid id")
+        }
+        throw err.name || new DatabaseError("Problem finding note")
+    })
+
+
+};
+
+updateNote = async (id, reqContent) => {
+    if(reqContent === null) {
+        return new ValidationError("No content given");
+    }
+    return Note.findByIdAndUpdate(id, { content: reqContent }, { new: true, useFindAndModify: false})
+    .then(note => {
+        // how to throw this error?
+        if(note === null) {
+            throw new ValidationError("No note with id: " + id);
+        } 
+        return note;
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            throw new ValidationError("invalid id")
+        }
+        throw err.name || new DatabaseError("Problem updating note")
+    })
+
+};
+
+deleteNotes = async () => {
+    return Note.deleteMany()
+    .catch(err => {
+        throw err.name || new DatabaseError("Problem deleting all notes");
+    })
+};
+
+deleteNote = async (id) => {
+    return Note.findByIdAndDelete(id)
+    .then(note => {
+        if(note === null) {
+            throw new ValidationError("No note with id: " + id);
+        } 
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            throw new ValidationError("invalid id")
+        }
+        throw err.name || new DatabaseError("Problem deleting note")
+    })
+        
+
+};
+
+
+
+
+
+module.exports = {
+    DatabaseError: DatabaseError,
+    ValidationError: ValidationError,
+    Note: Note,
+    createNote: createNote,
+    findNotes: findNotes,
+    findNote: findNote,
+    updateNote: updateNote,
+    deleteNotes: deleteNotes,
+    deleteNote: deleteNote
+}
+
